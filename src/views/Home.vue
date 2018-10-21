e4777811df9e5e52861ad36fd37e94a2418f6e5f
Nakamal <nickhaugland@gmail.com>
2018-10-21 11:17:11 -0500

    added login and signup pages as well as update main.js for axios and tokens

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+var axios = require('axios');
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Winston's Wacky & Wonderous Items"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
 </script>
